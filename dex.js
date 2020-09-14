893bc6b3edbe56b1121d4337bbdc4a846f0c5e23
Beardificent <cis.magito@gmail.com>
2020-09-14 18:10:30 +0200

    right side screen has a list of 20 pokemon, will work on showing next 20 pokemon

--- dex.js
+++ dex.js
@@ -1,3 +1,4 @@
+//DOM OBJECTS
 const mainScreen = document.querySelector('.main-screen');
 const pokeName = document.querySelector('.poke-name');
 const pokeId = document.querySelector('.poke-id');
@@ -7,10 +8,12 @@ const pokeTypeOne = document.querySelector('.poke-type-one');
 const pokeTypeTwo = document.querySelector('.poke-type-two');
 const pokeWeight = document.querySelector('.poke-weight');
 const pokeHeight = document.querySelector('.poke-height');
-const pokeListItems = document.querySelector('.list-item');
+const pokeListItems = document.querySelectorAll('.list-item');
 const leftButton = document.querySelector('.left-button');
 const rightButton = document.querySelector('.right-button');
-//constants and variables
+
+//CONSTANTS & VARIABLES
+
 const TYPES = [
     'normal', 'fighting', 'flying', 'poison', 'ground',
     'rock', 'ghost', 'bug', 'steel', 'fire', 'water',
@@ -30,6 +33,7 @@ const resetScreen = () => {
 mainScreen.classList.remove(type);
     }
 };
+//Get Data leftside of screen (main screen)
 
 fetch('https://pokeapi.co/api/v2/pokemon/1')
     .then(response => response.json())
@@ -53,11 +57,40 @@ fetch('https://pokeapi.co/api/v2/pokemon/1')
 
 
         pokeName.textContent = capitalize(data['name']);
-        pokeId.textContent = data['id'];
+        //We change the id to a string and with padStart we say that it needs to be 3 digits. If no digit is present, replace with '0'
+        pokeId.textContent = '#' + data['id'].toString().padStart(3, '0');
         pokeWeight.textContent = data['weight'];
         pokeHeight.textContent = data['height'];
-
-
         pokeFrontImage.src = data['sprites']['front_default'] || '';
         pokeBackImage.src = data['sprites']['back_default'] || '';
     });
+
+//Get data right side of screen.
+fetch('https://pokeapi.co/api/v2/pokemon?offset=0&limit=20')
+    .then(response => response.json())
+    .then(data => {
+        //const { results } = data; IS THE SAME THING, CALLED DESTRUCTURING
+        const results = data ['results'];
+
+
+   //Loop through Pokelistitems and get an item.
+        for( let i = 0; i < pokeListItems.length ; i++){
+            const pokeListItem = pokeListItems[i];
+            const resultData = results[i];
+
+
+
+            if (resultData){
+                //const name = resultData; IS THE SAME THING BUT NOT DESTRUCTURED
+                const { name, url } = resultData;
+                //THIS WILL MAKE THE URL SPLIT AFTER EVERY '/' AND THUS CREATING A LIST IF ELEMENTS WE CAN USE (SECOND TO LAST IS THE (pokemon)INDEX for example°
+                const urlArray = url.split('/');
+                const id = urlArray[urlArray.length - 2];
+                pokeListItem.textContent = id + '. ' + capitalize(name);
+
+            }else {
+                pokeListItem.textContent = '';
+
+            }
+        }
+    })
